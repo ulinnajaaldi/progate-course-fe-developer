e423ebc05892ca491bf255095b2c1c10c099d9ca
Ulinnaja Aldi <ulinnajaaldi24@gmail.com>
2023-08-30 00:22:54 +0700

    feat(kanban): develop complete feature

--- kanban-project/src/features/tasks/components/TaskList/TaskListItem.tsx
+++ kanban-project/src/features/tasks/components/TaskList/TaskListItem.tsx
@@ -1,8 +1,26 @@
+import React from 'react'
 import type { Task, CSSProperties } from '../../../../types'
 import { TASK_PROGRESS_STATUS, TASK_PROGRESS_ID } from '../../../../constants/app'
+import { useRecoilState } from 'recoil'
+import { tasksState } from '../../TaskAtoms'
 
 interface TaskListItemProps {
   task: Task
+}
+
+const getIconStyle = (progressOrder: number): React.CSSProperties => {
+  const color: '#55C89F' | '#C5C5C5' =
+    progressOrder === TASK_PROGRESS_ID.COMPLETED ? '#55C89F' : '#C5C5C5'
+
+  const cursor: 'default' | 'pointer' =
+    progressOrder === TASK_PROGRESS_ID.COMPLETED ? 'default' : 'pointer'
+
+  return {
+    color,
+    cursor,
+    fontSize: '28px',
+    marginRight: '6px',
+  }
 }
 
 const getProgressCategory = (progressOrder: number): string => {
@@ -21,10 +39,27 @@ const getProgressCategory = (progressOrder: number): string => {
 }
 
 const TaskListItem = ({ task }: TaskListItemProps) => {
+  const [tasks, setTasks] = useRecoilState<Task[]>(tasksState)
+
+  const completeTask = (taskId: number): void => {
+    const updateTask = tasks.map((task) =>
+      task.id === taskId ? { ...task, progressOrder: TASK_PROGRESS_ID.COMPLETED } : task,
+    )
+    setTasks(updateTask)
+  }
+
   return (
     <div style={styles.tableBody}>
       <div style={styles.tableBodyTaskTitle}>
-        <span className="material-icons">check_circle</span>
+        <span
+          className="material-icons"
+          style={getIconStyle(task.progressOrder)}
+          onClick={(): void => {
+            completeTask(task.id)
+          }}
+        >
+          check_circle
+        </span>
         {task.title}
       </div>
       <div style={styles.tableBodyDetail}>{task.detail}</div>
